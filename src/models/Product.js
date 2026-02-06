d95a64a6e1375a4960c47bfb8252ac745516f866
mathias ortiz <math@mathiass-MacBook-Air.local>
2026-02-06 10:33:33 +0100

    Creacion de modelo

---
+++ src/models/Product.js
@@ -0,0 +1,55 @@
+// ## Creación de modelo
+
+//Vamos a crear el modelo de producto. El modelo de producto tendrá los siguientes campos:
+
+//- Nombre
+//- Descripción
+//- Imagen
+//- Categoría
+//- Talla
+//- Precio
+
+//La categoría será un string que podrá ser "Camisetas", "Pantalones", "Zapatos", "Accesorios".
+
+//La talla será un string que podrá ser "XS", "S", "M", "L", "XL".
+
+const mongoose = require('mongoose');
+
+const validCategorias = ['Camisetas', 'Pantalones', 'Pantalones', 'Zapatos', 'Accesorios'];  // enum de colores
+const validTallas = ['XS', 'S', 'M', 'L', 'XL'];  
+
+const productSchema = new mongoose.Schema({
+    nombre: {
+        type : String,
+        requiered : true,
+        unique : true,
+    },
+    description: {
+        type : String,
+        requiered : true,
+    },
+    image: {
+        type : String,
+        requiered : true,
+    },
+    categoria: {
+        type : String,
+        requiered : true,
+        enum: validCategorias,
+    },
+    talla: {
+        type : String,
+        requiered : true,
+        enum: validTallas,
+    },
+    precio: {
+        type : Number,
+        requiered : true,
+    }
+})
+
+module.exports = mongoose.model('Product', productSchema);
+module.exports.validCategorias = validCategorias;
+module.exports.validTallas = validTallas;
+
+
